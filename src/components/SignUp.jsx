ccdb38002ee1d2129b74781611914e26d1d2b2c4
Krishanu <krishgoswami15@gmail.com>
2023-09-03 11:56:41 +0530

    finished user registration

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,42 +1,97 @@
-import { Button, Card, TextField, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
+import { Button, TextField, Typography, Box } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
-    return <div>
-        <center>
-            <div
-                style={{
-                    marginTop: 100,
-                    marginBottom: 20
-                }}>
-                <Typography variant='h6'>Welcome to Blog App. Sign up today</Typography>
-            </div>
-        </center>
-        <center>
-            <Card
-                variant='outlined'
-                style={{
-                    width: 400,
-                    padding: 20,
-                    borderRadius: 10
-                }}>
+    const navigate = useNavigate();
+
+    //state
+    const [inputs, setInputs] = useState({
+        username: '',
+        email: '',
+        password: ''
+    })
+
+    //handle input Change
+    const handleChange = (e) => {
+        setInputs(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    //handleSubmit
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const { data } = await axios.post("http://localhost:8080/api/v1/user/signup", {
+                username: inputs.username,
+                email: inputs.email,
+                password: inputs.password
+            })
+            if (data.success) {
+                alert('User registered successfully');
+                navigate('/login');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return <>
+        <form onSubmit={handleSubmit}>
+            <Box
+                maxWidth={450}
+                display='flex'
+                flexDirection={'column'}
+                alignItems={'center'}
+                justifyContent={'center'}
+                margin={'auto'}
+                marginTop={10}
+                boxShadow={'0px 0px 5px #ccc'}
+                padding={3}
+                borderRadius={3}
+                backgroundColor={'white'}
+            >
+                <Typography
+                    variant='h6'
+                    marginBottom={3}
+                >Welcome to Blog App. Sign up today.</Typography>
                 <TextField
                     fullWidth={true}
                     variant='outlined'
                     label='Name'
-                /><br /><br />
+                    name='username'
+                    required
+                    value={inputs.username}
+                    onChange={handleChange}
+                /><br />
                 <TextField
                     fullWidth={true}
                     variant='outlined'
+                    type='email'
                     label='Email'
-                /><br /><br />
+                    name='email'
+                    required
+                    value={inputs.email}
+                    onChange={handleChange}
+                /><br />
                 <TextField
                     fullWidth={true}
                     variant='outlined'
                     label='Password'
+                    name='password'
                     type='password'
-                /><br /><br />
-                <Button variant='contained'>Sign Up</Button><br /><br />
+                    required
+                    value={inputs.password}
+                    onChange={handleChange}
+                /><br />
+                <Button
+                    variant='contained'
+                    type='submit'
+                >Sign Up</Button><br />
                 <div
                     style={{
                         display: 'flex',
@@ -45,13 +100,14 @@ export default function SignUp() {
                     }}>
                     <Typography>Have an account already?</Typography>
                     <Button
+                        sx={{ "&:hover": { backgroundColor: "transparent", } }}
+                        disableRipple={true}
                         variant='text'
                         LinkComponent={Link}
                         to='/login'
                     >Login</Button>
-                    {/* <Link href="/login">Login</Link> */}
                 </div>
-            </Card>
-        </center>
-    </div>
+            </Box>
+        </form>
+    </>
 }
