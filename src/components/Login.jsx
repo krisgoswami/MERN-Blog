ec49fcabc4e48d75032c4404f8a3fd93fe181911
Krishanu <krishgoswami15@gmail.com>
2023-09-03 12:31:18 +0530

    finished user login function

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,52 +1,107 @@
-import { Button, Card, TextField, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Button, Card, TextField, Typography, Box } from '@mui/material';
+import axios from 'axios';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { authActions } from '../redux/store';
+
 
 export default function Login() {
-    return <div>
-        <center>
-            <div
-                style={{
-                    marginTop: 100,
-                    marginBottom: 20
-                }}>
-                <Typography variant='h6'>Welcome to Blog App. Login Below</Typography>
-            </div>
-        </center>
-        <center>
-            <Card
-                variant='outlined'
-                style={{
-                    width: 400,
-                    padding: 20,
-                    borderRadius: 10
-                }}>
-                <TextField
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    //state
+    const [inputs, setInputs] = useState({
+        email: '',
+        password: '',
+    })
+
+    //handle on change
+    const handleChange = (e) => {
+        setInputs(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    //handle submit
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const { data } = await axios.post('http://localhost:8080/api/v1/user/login', {
+                email: inputs.email,
+                password: inputs.password
+            })
+            if (data.success) {
+                dispatch(authActions.login());
+                navigate('/')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return <>
+        <form onSubmit={handleSubmit}>
+            <Box
+                maxWidth={450}
+                display='flex'
+                flexDirection={'column'}
+                alignItems={'center'}
+                justifyContent={'center'}
+                margin={'auto'}
+                marginTop={10}
+                boxShadow={'0px 0px 5px #ccc'}
+                padding={3}
+                borderRadius={3}
+                backgroundColor={'white'}
+            >
+                <Typography
+                    variant='h6'
+                    marginBottom={3}
+                >Welcome. Please Login Below.</Typography>
+
+                < TextField
                     fullWidth={true}
                     variant='outlined'
                     label='Email'
-                /><br /><br />
+                    name='email'
+                    type='email'
+                    required
+                    value={inputs.email}
+                    onChange={handleChange}
+                /><br />
                 <TextField
                     fullWidth={true}
                     variant='outlined'
                     label='Password'
+                    name='password'
                     type='password'
-                /><br /><br />
-                <Button variant='contained'>Login</Button><br /><br />
-                <div
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center'
-                    }}>
+                    required
+                    value={inputs.password}
+                    onChange={handleChange}
+                /><br />
+                <Button
+                    variant='contained'
+                    type='submit'
+                >Login</Button><br />
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
                     <Typography>Don't have an account?</Typography>
                     <Button
+                        sx={{ "&:hover": { backgroundColor: "transparent", } }}
+                        disableRipple={true}
                         variant='text'
                         LinkComponent={Link}
                         to='/signup'
                     >Sign Up</Button>
-                    {/* <Link href="/signup">Sign Up</Link> */}
                 </div>
-            </Card>
-        </center>
-    </div>
+            </Box>
+        </form>
+    </ >
 }
